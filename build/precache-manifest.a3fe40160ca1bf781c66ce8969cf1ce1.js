d6d05b3991da1504b7a891df98d7fed7f85ff85d
Alexander-Cui <43623796+Alexander-Cui@users.noreply.github.com>
2020-08-09 18:31:22 -0400

    Add files via upload

---
+++ build/precache-manifest.a3fe40160ca1bf781c66ce8969cf1ce1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cf4f43c31be6119d529282311c0727f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2bffa2e7d9005d06b64",
+    "url": "/static/css/2.4c4477be.chunk.css"
+  },
+  {
+    "revision": "bf1441c539403c1f976f",
+    "url": "/static/css/main.7e4b5e62.chunk.css"
+  },
+  {
+    "revision": "f2bffa2e7d9005d06b64",
+    "url": "/static/js/2.34b58faa.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.34b58faa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf1441c539403c1f976f",
+    "url": "/static/js/main.b0e6cbd4.chunk.js"
+  },
+  {
+    "revision": "c769a5205bd4da41bd84",
+    "url": "/static/js/runtime-main.c83b5dc9.js"
+  }
+]);
